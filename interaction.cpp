55b8e6e433fc2fad5b206a4ca5de6a2d298fa6d5
natsukmoe <charlieyan@126.com>
2020-04-10 22:20:53 +0800

    27

--- interaction.cpp
+++ interaction.cpp
@@ -15,14 +15,22 @@ void StartInteract(){
     string Command;
     int curpos=0;
     vector<char> ram(30000,0);
+    vector<int> cycpos;
     while(1){
-        printf("Natsubf:%d(%d)> ",curpos,ram[curpos]);
-        getline(cin,Command);
+        int Val=ram[curpos];
+        if(Val<0){
+            Val+=256;
+        }
+        printf("Natsubf:%d(%d)> ",curpos,Val);
+        if(!getline(cin,Command)){
+            puts("");
+            break;
+        }
         int khpip=0;
         for(int i=0;i<(int)Command.size();i++){
-            if(Command[i]=='('){
+            if(Command[i]=='['){
                 khpip++;
-            }else{
+            }else if(Command[i]==']'){
                 khpip--;
                 if(khpip<0){
                     break;
@@ -39,9 +47,9 @@ void StartInteract(){
             string s;
             getline(cin,s);
             for(int i=0;i<(int)s.size();i++){
-                if(s[i]=='('){
+                if(s[i]=='['){
                     khpip++;
-                }else{
+                }else if(s[i]==']'){
                     khpip--;
                     if(khpip<0){
                         break;
@@ -57,6 +65,59 @@ void StartInteract(){
         if(tbc){
             puts("Execution failed! Unmatched right parenthesis found!");
             continue;
+        }
+        bool isinp=0;
+        for(int i=0;i<(int)Command.size();i++){
+            if(Command[i]=='<'){
+                if(curpos==0){
+                    puts("Runtime Error! You executed a '<' at position 0!");
+                    break;
+                }
+                curpos--;
+            }else if(Command[i]=='>'){
+                if(curpos==29999){
+                    puts("Runtime Error! You executed a '>' at position 29999!");
+                    break;
+                }
+                curpos++;
+            }else if(Command[i]==','){
+                ram[curpos]=getchar();
+                isinp=1;
+            }else if(Command[i]=='.'){
+                putchar(ram[curpos]);
+            }else if(Command[i]=='+'){
+                ram[curpos]++;
+            }else if(Command[i]=='-'){
+                ram[curpos]--;
+            }else if(Command[i]=='['){
+                if(!ram[i]){
+                    khpip=1;
+                    while(khpip){
+                        i++;
+                        if(Command[i]=='['){
+                            khpip++;
+                        }else if(Command[i]==']'){
+                            khpip--;
+                        }
+                    }
+                }else{
+                    cycpos.push_back(i);
+                }
+            }else if(Command[i]==']'){
+                if(!ram[i]){
+                    cycpos.pop_back();
+                }else{
+                    i=cycpos.back();
+                }
+            }else if(Command[i]=='*'){
+                ram.assign(30000,0);
+                curpos=0;
+            }else if(Command[i]=='$'){
+                exit(0);
+            }
+        }
+        if(isinp){
+            while(getchar()!='\n');
         }
     }
 }

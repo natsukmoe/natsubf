3a5d784273a475820794443435955c28a31b6184
natsukmoe <charlieyan@126.com>
2020-04-17 09:36:55 +0800

    36
    Wanna optimize the program

---
+++ optimizer.cpp
@@ -0,0 +1,13 @@
+//
+//  optimizer.cpp
+//  natsubf
+//
+//  Created by 夏欣萌 on 2020/4/17.
+//  Copyright © 2020 夏欣萌. All rights reserved.
+//
+
+#include "headers.h"
+
+namespace{
+vector<int> cmds;
+}

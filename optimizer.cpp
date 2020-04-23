1d05464b9bcdf647bbb7b7861aad71101bce40be
natsukmoe <charlieyan@126.com>
2020-04-23 16:50:13 +0800

    43
    Optimizer完成！

--- optimizer.cpp
+++ optimizer.cpp
@@ -77,4 +77,51 @@ void optimizeProgram(vector<string> &files){
             cmds.push_back(6);
         }
     }
+    int cur=0;
+    vector<char> ram(30000,0),stk;
+    int Size=(int)cmds.size(),pos=0;
+    while(cur<Size){
+        if(cmds[cur]==1){
+            cur++;
+            ram[pos]+=cmds[cur];
+        }else if(cmds[cur]==2){
+            cur++;
+            pos+=cmds[cur];
+            if(pos<0){
+                fprintf(stderr,"Error: Memory out of bounds!\nYou executed a '<' at memory position 0!\n");
+                exit(1);
+            }
+            if(pos>29999){
+                fprintf(stderr,"Error: Memory out of bounds!\nYou executed a '>' at memory position 29999!\n");
+                exit(2);
+            }
+        }else if(cmds[cur]==3){
+            ram[pos]=getchar();
+        }else if(cmds[cur]==4){
+            putchar(ram[pos]);
+        }else if(cmds[cur]==5){
+            if(!ram[pos]){
+                int ceng=1;
+                while(ceng){
+                    cur++;
+                    if(cmds[cur]==5){
+                        ceng++;
+                    }else if(cmds[cur]==6){
+                        ceng--;
+                    }else if(cmds[cur]==1||cmds[cur]==2){
+                        cur++;
+                    }
+                }
+            }else{
+                stk.push_back(cur);
+            }
+        }else if(cmds[cur]==6){
+            if(!ram[pos]){
+                stk.pop_back();
+            }else{
+                cur=stk.back();
+            }
+        }
+        cur++;
+    }
 }
